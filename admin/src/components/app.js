755133d47992ec3cb83fe0c995d797110a9a5c38
Viktoras Kuznecovas <viktoras@kuznecovas.net>
2018-06-01 09:53:53 +0300

    oauth redirect change

--- admin/src/components/app.js
+++ admin/src/components/app.js
@@ -1,21 +1,12 @@
 import { h, Component } from 'preact';
 import Header from './header';
 import Panel from '../routes/panel';
-// import Home from 'async!../routes/home';
-// import Profile from 'async!../routes/profile';
 
 if (module.hot) {
 	require('preact/debug');
 }
 
 export default class App extends Component {
-	/** Gets fired when the route changes.
-	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-	 *	@param {string} event.url	The newly routed URL
-	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
 	render() {
 		return (
 			<div id="app">
