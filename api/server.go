6d0cb265874f1eb3dc31782160a4f0bb2c5c806d
Viktoras Kuznecovas <viktoras@kuznecovas.net>
2018-08-22 11:58:18 +0300

    removed pprof

--- api/server.go
+++ api/server.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/gin-contrib/cors"
-	"github.com/gin-contrib/pprof"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
@@ -81,7 +80,6 @@ func GetServer(db *abstraction.Database, config *model.Config) (*gin.Engine, err
 		r.Use(gin.Logger())
 	}
 	r.ForwardedByClientIP = true
-	pprof.Register(r, &pprof.Options{RoutePrefix: "debug/pprof"})
 	if config.API.Cors.Enabled {
 		corsConfig := cors.DefaultConfig()
 		corsConfig.AllowOrigins = *config.API.Cors.AllowedOrigins
