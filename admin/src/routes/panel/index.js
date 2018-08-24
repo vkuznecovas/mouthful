945ed450831df197c7a9ea6f088d3539c1125966
Viktoras Kuznecovas <viktoras@kuznecovas.net>
2018-08-24 15:13:48 +0300

    fix the sort

--- admin/src/routes/panel/index.js
+++ admin/src/routes/panel/index.js
@@ -149,7 +149,11 @@ export default class Panel extends Component {
 		if (!(this.state.threads && this.state.comments && this.state.threads.length && this.state.comments.length)) {
 			return <div class={style.mouthful_container}><div class={style.mouthful_login}>No comments yet!</div></div>
 		}
-		var threads = this.state.threads.map(t => {
+		var threads = this.state.threads.sort((a, b) => {
+			a = new Date(a.CreatedAt);
+			b = new Date(b.CreatedAt);
+			return a>b ? -1 : a<b ? 1 : 0;
+		}).map(t => {
 			var comments = this.state.comments
 			const pendingFilter = x => {
 				return !x.Confirmed && x.DeletedAt == null
@@ -172,11 +176,7 @@ export default class Panel extends Component {
 				return <Thread url={getUrl(this.state, window)} key={"___thread" + t.Id} thread={t} comments={c} reload={this.reload} updateComment={this.updateComment}/>
 			}
 			return null;
-		}).sort(function(a, b) {
-			a = new Date(a.CreatedAt);
-			b = new Date(b.CreatedAt);
-			return a>b ? -1 : a<b ? 1 : 0;
-		});
+		})
 		var resultDiv = threads.filter(x => x != null).length > 0 ? threads : <div class={style.nothing}>Nothing to display</div>
 		return (
 			<div class={style.mouthful_container}>
